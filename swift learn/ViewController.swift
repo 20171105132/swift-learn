e4f7e4d8776f6378906a27f9dd872a7fb7e66ce3
张悦 <1647160039@qq.com>
2018-10-11 09:57:38 +0800

    计算器程序
    加了小数的加减

--- swift learn/ViewController.swift
+++ swift learn/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var temp=0
-    var sum=0
+    var temp=0.0
+    var sum=0.0
     var flag=0
     @IBOutlet weak var Caculator: UITextField!
     
@@ -41,42 +41,49 @@ class ViewController: UIViewController {
     @IBAction func button9(_ sender: Any) {
         Caculator.text=Caculator.text!+"9"
     }
+    @IBAction func button10(_ sender: Any) {
+        Caculator.text=Caculator.text!+"."
+    }
+    @IBAction func button0(_ sender: Any) {
+        Caculator.text=Caculator.text!+"0"
+    }
+    
     @IBAction func add(_ sender: Any) {
-        temp=Int(Caculator.text!)!
+        temp=Double(Caculator.text!)!
         Caculator.text=""
         flag=1
     }
     @IBAction func Reduce(_ sender: Any) {
-        temp=Int(Caculator.text!)!
+        temp=Double(Caculator.text!)!
         Caculator.text=""
         flag=2
     }
     @IBAction func Take(_ sender: Any) {
-        temp=Int(Caculator.text!)!
+        temp=Double(Caculator.text!)!
         Caculator.text=""
         flag=3
     }
     @IBAction func division(_ sender: Any) {
-        temp=Int(Caculator.text!)!
+        temp=Double(Caculator.text!)!
         Caculator.text=""
         flag=4
     }
     @IBAction func equal(_ sender: Any) {
         
         if(flag==1){
-            sum=temp+Int(Caculator.text!)!
+            sum=temp+Double(Caculator.text!)!
             Caculator.text!="\(sum)"
         }
         if(flag==2){
-            sum=temp-Int(Caculator.text!)!
+            sum=temp-Double(Caculator.text!)!
             Caculator.text!="\(sum)"
         }
         if(flag==3){
-            sum=temp*Int(Caculator.text!)!
+            sum=temp*Double(Caculator.text!)!
             Caculator.text!="\(sum)"
         }
         if(flag==4){
-            sum=temp/Int(Caculator.text!)!
+            sum=temp/Double(Caculator.text!)!
             Caculator.text!="\(sum)"
         }
     }
