adc961293842dcf070a0b83bf318eea678efe2a7
张悦 <1647160039@qq.com>
2018-11-29 09:03:18 +0800

    计算器
    新加了个文本框

--- swift learn/ViewController.swift
+++ swift learn/ViewController.swift
@@ -5,6 +5,23 @@
 //  Created by zhangyue on 2018/10/11.
 //  Copyright © 2018年 zhangyue. All rights reserved.
 //
+struct Stack<T> {
+    fileprivate var array: [T] = []
+    
+    public var count: Int {
+        return array.count
+    }
+    public mutating func push(_ element: T) {
+        array.append(element)
+    }
+    public mutating func pop() -> T? {
+        return array.popLast()
+    }
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+}
 
 import UIKit
 
@@ -17,104 +34,128 @@ class ViewController: UIViewController {
     var point=false  //判断小数点是否存在
     var caflag=0    //判断数前面是符号还是数字
     @IBOutlet weak var Caculator: UITextField!
-   
+    @IBOutlet weak var display: UITextField!
     @IBAction func button1(_ sender: Any) {
         if(caflag==1){
             Caculator.text=Caculator.text!+"1"
+            display.text=display.text!+"1"
         }else{
             Caculator.text="1"
+            display.text="1"
         }
         caflag=1
     }
     @IBAction func button2(_ sender: Any) {
         if(caflag==1){
             Caculator.text=Caculator.text!+"2"
+            display.text=display.text!+"2"
         }else{
             Caculator.text="2"
+            display.text="2"
         }
         caflag=1
     }
     @IBAction func button3(_ sender: Any) {
         if(caflag==1){
             Caculator.text=Caculator.text!+"3"
+            display.text=display.text!+"3"
         }else{
             Caculator.text="3"
+            display.text="3"
         }
         caflag=1
     }
     @IBAction func button4(_ sender: Any) {
         if(caflag==1){
             Caculator.text=Caculator.text!+"4"
+            display.text=display.text!+"4"
         }else{
             Caculator.text="4"
+            display.text="4"
         }
         caflag=1
     }
     @IBAction func button5(_ sender: Any) {
         if(caflag==1){
             Caculator.text=Caculator.text!+"5"
+            display.text=display.text!+"5"
         }else{
             Caculator.text="5"
+            display.text="5"
         }
         caflag=1
     }
     @IBAction func button6(_ sender: Any) {
         if(caflag==1){
             Caculator.text=Caculator.text!+"6"
+            display.text=display.text!+"6"
         }else{
             Caculator.text="6"
+            display.text="6"
         }
         caflag=1
     }
     @IBAction func button7(_ sender: Any) {
         if(caflag==1){
             Caculator.text=Caculator.text!+"7"
+            display.text=display.text!+"7"
         }else{
             Caculator.text="7"
+            display.text="7"
         }
         caflag=1
     }
     @IBAction func button8(_ sender: Any) {
         if(caflag==1){
             Caculator.text=Caculator.text!+"8"
+            display.text=display.text!+"8"
         }else{
             Caculator.text="8"
+            display.text="8"
         }
         caflag=1
     }
     @IBAction func button9(_ sender: Any) {
         if(caflag==1){
             Caculator.text=Caculator.text!+"9"
+            display.text=display.text!+"9"
         }else{
             Caculator.text="9"
+            display.text="9"
         }
         caflag=1
     }
     @IBAction func button10(_ sender: Any) {
         Caculator.text=Caculator.text!+"."
+        display.text=display.text!+"."
         point=true
     }
     @IBAction func button0(_ sender: Any) {
         if(caflag==1){
             Caculator.text=Caculator.text!+"0"
+            display.text=display.text!+"0"
         }else{
             Caculator.text="0"
+            display.text="0"
         }
         caflag=1
     }
     
     @IBAction func mins_sign(_ sender: Any) {
         Caculator.text!="-"
+        display.text="-"
     }
     
     @IBAction func add(_ sender: Any) {
         if(caflag==0)
         {
             Caculator.text="0"
+            display.text="0"
         }
         temp=Double(Caculator.text!)!
         if(i==0){
             Caculator.text=""
+            display.text=display.text!+"+"
             sum=temp
         }
          i=i+1
@@ -123,11 +164,14 @@ class ViewController: UIViewController {
             {
                 sum=sum+temp
                 Caculator.text=""
+                display.text=display.text!+"+"
                 
             }
             if(flag==2)
             {
                 sum=sum-temp
+                Caculator.text=""
+                display.text=display.text!+"+"
             }
             Caculator.text=""
         }
@@ -139,10 +183,12 @@ class ViewController: UIViewController {
         if(caflag==0)
         {
             Caculator.text="0"
+            display.text="0"
         }
         temp=Double(Caculator.text!)!
         if(i==0){
             Caculator.text=""
+            display.text=display.text!+"-"
             sum=temp
         }
         i=i+1
@@ -150,10 +196,14 @@ class ViewController: UIViewController {
             if(flag==1)
             {
                 sum=sum+temp
+                Caculator.text=""
+                display.text=display.text!+"-"
             }
             if(flag==2)
             {
                 sum=sum-temp
+                Caculator.text=""
+                display.text=display.text!+"-"
             }
             Caculator.text=""
         }
@@ -165,16 +215,19 @@ class ViewController: UIViewController {
         if(caflag==0)
         {
             Caculator.text="0"
+            display.text="0"
         }
          temp=Double(Caculator.text!)!
         if(i==0){
             Caculator.text=""
+            display.text=display.text!+"*"
             sum=temp
         }
         i=i+1
         if(i==2){
             sum=sum*Double(Caculator.text!)!
             Caculator.text=""
+            display.text=display.text!+"*"
         }
         i=1
         flag=3
@@ -184,21 +237,25 @@ class ViewController: UIViewController {
         if(caflag==0)
         {
             Caculator.text="0"
+            display.text="0"
         }
         temp=Double(Caculator.text!)!
         if(i==0){
             Caculator.text=""
+            display.text=display.text!+"/"
             sum=temp
         }
         i=i+1
         if(i==2){
             sum=sum/(Double(Caculator.text!)!)
+            display.text=display.text!+"/"
             Caculator.text=""
         }
         i=1
         flag=4
         if(temp==0){
             Caculator.text="error"
+            display.text="error"
         }
     }
 
@@ -258,11 +315,12 @@ class ViewController: UIViewController {
     }
     @IBAction func button_clear(_ sender: Any) {
         Caculator.text=""
+        display.text=""
         sum=0
         sum1=0
         point=false
     }
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
